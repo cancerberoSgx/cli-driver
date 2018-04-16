fb6235e0c49c3c19b775e95f5f8745db58bc0f23
Sebastián Gurin <sebastigurin@gmail.com>
2018-04-16 18:00:26 -0300

    docs update

--- packages/cli-driver/typedoc.ts
+++ packages/cli-driver/typedoc.ts
@@ -2,7 +2,7 @@ module.exports = {
     src: [
         './lib/src/',
         './node_modules/ansi-escape-sequences/types/ansi-escape-sequences.d.ts',
-        "./node_modules/node-pty/typings/node-pty.d.ts"
+        './node_modules/node-pty/typings/node-pty.d.ts'
     ],
     "mode": "file",
     "includeDeclarations": true,
